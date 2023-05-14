e50e955d17eca00683f95cc66c7e4cc532389c1b
Rod-Vadar <105311434+Rod-Vadar@users.noreply.github.com>
2023-05-14 09:30:36 +0000

    Add django forms

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from app.models import JobPost
+from app.models import Author, JobPost, Location, Skills
 
 class JobAdmin(admin.ModelAdmin):
     list_display= ('__str__','title','salary','date',)
@@ -21,3 +21,6 @@ class JobAdmin(admin.ModelAdmin):
     
 # Register your models here.
 admin.site.register(JobPost)
+admin.site.register(Location)
+admin.site.register(Author)
+admin.site.register(Skills)
